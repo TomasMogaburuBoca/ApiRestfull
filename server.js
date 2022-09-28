881b182c357b9f63934923df468d212494a09f5a
Tomas Mogaburu Boca <tomasmogaburu@gmail.com>
2022-09-28 20:58:03 -0300

    Segundo commit con ApiRestfull completa

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const routerProduct = Router();
 const app = express();
 
 app.use('/api', routerProduct);
-
+routerProduct.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 const products = [
     {product : 'Fish',
@@ -23,27 +25,50 @@ routerProduct.get ('/products', (req, res) =>{
     let newProducts = [...products]
     res.json(newProducts)
 })
-routerProduct.get ('/products/:id', (req, res, id) =>{
-    let getId = req.params
-    if(getId == products.id){
-        res.json({id: `${products.product} ${products.id}`});
-    }else {return  undefined};
+routerProduct.get ('/products/:id', (req, res,) =>{
+    const numId = parseInt(req.params.id)
+    res.json(getById(numId))
 })
 
 routerProduct.post ('/products', (req, res) =>{
     let addProducts = req.body;
-    let newProduct = addProducts.push(products);
-    res.json(newProduct)
+    products.push(addProducts)
+    res.json(products);
 })
-console.log(products);
 
 routerProduct.put('/products/:id', (req, res)=>{
+    const num = parseInt(req.params.id)
+    const product = req.body
+    res.json(changeById(num, products))
 })
 
 routerProduct.delete('/products/:id', (req, res) =>{
+    const numId = parseInt(req.params.id)
+    res.json(deleteById(numId))
 })
+
+
+
+getById = (id) =>{
+    const findId = products.find( prod => prod.id == id)
+    console.log(findId);
+    if (findId){
+        return products.id;
+    }else return null;
+}
+changeById = (id, products) =>{
+    products.id = id;
+    const changeId = products.splice(0, 0, products);
+    return products.id;
+}
+
+deleteById = (id) =>{
+    products.find ( prod => prod.id !== id);
+    return ({msg: `The products ${id} was deleted`})
+}
 
 const PORT = 8080;
 
 const server = app.listen (PORT, () => console.log(`running in ${PORT}`));
 server.on('error', error => console.log(error));
+
